21664324837fa357dbd39e87111ef7a016598a7e
Francisco Claudino <61290111+Francisco658@users.noreply.github.com>
2024-05-10 13:46:12 +0100

    Cleaning

--- app/src/main/java/com/example/sa/AccelerometerViewModel.kt
+++ app/src/main/java/com/example/sa/AccelerometerViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class AccelerometerViewModel: ViewModel() {
-    // Create a LiveData object with a AccelerometerData object
+    
     val currentAccelerometerData: MutableLiveData<AccelerometerData> by lazy {
         MutableLiveData<AccelerometerData>()
     }
