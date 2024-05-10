4d4f7544f212fd8a1e2fe189287de53db22bf279
Francisco Claudino <61290111+Francisco658@users.noreply.github.com>
2024-05-10 13:50:21 +0100

    Cleaning

--- app/src/main/java/com/example/sa/GyroscopeViewModel.kt
+++ app/src/main/java/com/example/sa/GyroscopeViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GyroscopeViewModel: ViewModel() {
-    // Create a LiveData object with a AccelerometerData object
+    
     val currentGyroscopeData: MutableLiveData<GyroscopeData> by lazy {
         MutableLiveData<GyroscopeData>()
     }
