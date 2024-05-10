049d4fccbfba027db824921dcf633e00c11b3fd3
Francisco Claudino <61290111+Francisco658@users.noreply.github.com>
2024-05-10 14:06:34 +0100

    Cleaning

--- app/src/main/java/com/example/sa/ShootActivity.kt
+++ app/src/main/java/com/example/sa/ShootActivity.kt
@@ -49,7 +49,6 @@ class ShootActivity : AppCompatActivity() {
 
         mediaPlayerbip = MediaPlayer.create(this, R.raw.countdown)
 
-        // Initialize Firebase Auth
         auth = com.google.firebase.ktx.Firebase.auth
 
         auth.uid?.let { lerpeso(it) }
@@ -81,9 +80,6 @@ class ShootActivity : AppCompatActivity() {
                     vibrator.vibrate(1000)
                     val formattedTime = String.format("%02d:%02d", 0, 0)
                     findViewById<TextView>(R.id.timerdisplay).text = formattedTime
-                    //alertDialog.setMessage("Parabéns")
-                    //alertDialog.show() // Mostrar a caixa de diálogo
-                    //mediaPlayer.start()
                     criarRemate()
                 }
             }.start()
@@ -116,20 +112,17 @@ class ShootActivity : AppCompatActivity() {
         val gyroscopeSensorListener = GyroscopeSensorListener()
 
         var novoRemateId:String= ""
-        // Declare um Handler e um Runnable
+
         val handler = Handler()
 
         val stopSensorTask = Runnable {
-            // Pare de coletar os dados do acelerômetro
             sensorManager.unregisterListener(accelerometerSensorListener)
             sensorManager.unregisterListener(gyroscopeSensorListener)
-            //lerDocs(novoRemateId)
             ler2Docs(novoRemateId)
         }
 
         val currentUser = auth.currentUser
 
-        // Create a new user with a first and last name
         val novoSaltoData  = hashMapOf(
             "user_id" to currentUser?.uid,
             "força" to 0,
@@ -139,10 +132,8 @@ class ShootActivity : AppCompatActivity() {
 
         val novoRemateRef = db.collection("Shoot").add(novoSaltoData)
             .addOnSuccessListener { documentReference ->
-                // O documento foi adicionado com sucesso, você pode acessar o ID aqui
                 novoRemateId = documentReference.id
                 Log.d("REMATE333", "Novo ID de Salto: $novoRemateId")
-                // Inicie a coleta dos dados do acelerômetro
                 mAccelerometer?.let { accelerometer ->
                     accelerometerSensorListener.setSensorManager(sensorManager, aViewModel,"Shoot",novoRemateId)
                     sensorManager.registerListener(
@@ -151,8 +142,7 @@ class ShootActivity : AppCompatActivity() {
                         SensorManager.SENSOR_DELAY_GAME
                     )
 
-                    // Defina um tempo para parar de coletar os dados (por exemplo, 1 segundos)
-                    handler.postDelayed(stopSensorTask, 2000) // 1000 milissegundos = 1 segundos
+                    handler.postDelayed(stopSensorTask, 2000) // 1000 milissegundos = 1 segundo
                 }
                 mGyroscope?.let { gyroscope ->
                     gyroscopeSensorListener.setSensorManager(sensorManager, aViewModel2,"Shoot",novoRemateId)
@@ -162,38 +152,33 @@ class ShootActivity : AppCompatActivity() {
                         SensorManager.SENSOR_DELAY_GAME
                     )
 
-                    // Defina um tempo para parar de coletar os dados (por exemplo, 1 segundos)
-                    handler.postDelayed(stopSensorTask, 2000) // 1000 milissegundos = 1 segundos
+                    handler.postDelayed(stopSensorTask, 2000) // 1000 milissegundos = 1 segundo
                 }
 
             }
             .addOnFailureListener { e ->
-                // Ocorreu um erro ao adicionar o documento
                 Log.e("SHOOT333", "Erro ao adicionar o documento", e)
             }
 
     }
 
     private fun ler2Docs(novoRemateId:String) {
-        // Primeira coleção
         db.collection("Shoot").document(novoRemateId)
             .collection("AccelerometerData")
-            .orderBy("timestamp") // Ordenar os documentos pelo campo "timestamp"
+            .orderBy("timestamp")
             .get()
             .addOnSuccessListener { result1 ->
                 val listaDeDados1 = result1.toObjects<AccelerometerData>()
 
-                // Segunda coleção
                 db.collection("Shoot").document(novoRemateId)
                     .collection("GyroscopeData")
-                    .orderBy("timestamp") // Ordenar os documentos pelo campo "timestamp"
+                    .orderBy("timestamp")
                     .get()
                     .addOnSuccessListener { result2 ->
                         val listaDeDados2 = result2.toObjects<GyroscopeData>()
 
-                        // Calcular pontuação com base nas duas listas de dados
                         val pontuacao = calcularpontuacao(listaDeDados1, listaDeDados2, novoRemateId)
-                        animarPontuacao(pontuacao) // Assumindo que a função animarPontuacao espera um valor de pontuação
+                        animarPontuacao(pontuacao)
 
                     }
                     .addOnFailureListener { exception ->
@@ -207,14 +192,13 @@ class ShootActivity : AppCompatActivity() {
 
     private fun lerpeso(novoSaltoId: String) {
         auth.uid?.let {
-            db.collection("users").document(it) // Ordenar os documentos pelo campo "timestamp"
+            db.collection("users").document(it)
                 .get()
                 .addOnSuccessListener { documentSnapshot ->
                     if (documentSnapshot.exists()) {
-                        val usuario = documentSnapshot.toObject<User>() // Classe Usuario
-                        peso = (usuario?.peso?: 0.0).toString().toFloat() // Peso do usuário (se existir)
+                        val usuario = documentSnapshot.toObject<User>()
+                        peso = (usuario?.peso?: 0.0).toString().toFloat()
 
-                        // Use o pesoUsuario para o cálculo da pontuação ou outras ações
                         Log.d("Peso", "User $peso")
                     } else {
                         Log.d("User555", "User document not found for ID:")
@@ -266,7 +250,6 @@ class ShootActivity : AppCompatActivity() {
     private fun calcularpontuacao(listaDeDadosA:List<AccelerometerData>, listaDeDadosG:List<GyroscopeData>, novoRemateId:String):Int{
         var pontos  = 800
 
-        // Encontre o elemento com o menor valor de accelerometerX
         val auxA = maxValorAcelerometro(listaDeDadosA)
         val auxG = maxValorGiroscopio(listaDeDadosG)
         val accelerometerData = listaDeDadosA[auxA]
@@ -276,28 +259,22 @@ class ShootActivity : AppCompatActivity() {
                 *accelerometerData.accelerometerY+accelerometerData.accelerometerZ*accelerometerData.accelerometerZ)
         var fG = sqrt(gyroscopeValue.gyroscopeX *gyroscopeValue.gyroscopeX+gyroscopeValue.gyroscopeY
                 *gyroscopeValue.gyroscopeY+gyroscopeValue.gyroscopeZ*gyroscopeValue.gyroscopeZ)
-        val forca = (fA * fG * peso*0.17) // *massa
+        val forca = (fA * fG * peso*0.17)
         pontos=calcularPontuacaocomF(forca)
 
-        // Referência para o documento que você deseja atualizar
         val docRef = db.collection("Shoot").document(novoRemateId)
 
-        // Atualiza o campo desejado
         docRef
             .update("força", forca)
             .addOnSuccessListener {
-                // Sucesso ao atualizar o campo
             }
             .addOnFailureListener { e ->
-                // Tratamento de erro
             }
         docRef
             .update("pontuação", pontos)
             .addOnSuccessListener {
-                // Sucesso ao atualizar o campo
             }
             .addOnFailureListener { e ->
-                // Tratamento de erro
             }
 
         return pontos
@@ -308,9 +285,7 @@ class ShootActivity : AppCompatActivity() {
             forca <= 0 -> 0
             forca >= 3600-> 999
             else -> {
-                // Aqui você pode definir uma relação entre a altura do salto e a pontuação
-                // por exemplo, você pode usar uma função linear, exponencial, etc.
-                val pontuacao = (forca*0.2775).toInt() // Ajustado para o máximo de 1.3 metros
+                val pontuacao = (forca*0.2775).toInt()
                 pontuacao
             }
         }
@@ -318,18 +293,14 @@ class ShootActivity : AppCompatActivity() {
 
     private fun animarPontuacao(pontuacao:Int) {
 
-        // Criar um ValueAnimator para animar a pontuação
         val animator = ValueAnimator.ofInt(0, pontuacao)
-        animator.duration = 2000 // Duração da animação em milissegundos
+        animator.duration = 2000
 
-        // Adicionar um listener de atualização de valor
         animator.addUpdateListener { animation ->
-            // Atualizar o TextView com o valor animado
             val valorAtual = animation.animatedValue as Int
             findViewById<TextView>(R.id.pontos).text = "$valorAtual"
         }
 
-        // Iniciar a animação
         animator.start()
     }
 }
